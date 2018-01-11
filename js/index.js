fdb201bdafc93c24bf14821e3e84b6e567a66add
kaikai <kaiqunliu@163.com>
2018-01-11 17:44:28 +0800

    用户界面

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+  $("#system").click(function(){
+		$(".systemCon").toggle();
+	})
+  
+  $(".systemConTop").click(function(){
+		$(".systemConBot").toggle();
+	})
+  
+   $(".jichuTit").click(function(){
+		$(".jichuCon").toggle();
+	})
+  
+});
